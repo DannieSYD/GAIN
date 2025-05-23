9eb5cea2872247f245e9b888241f49fee81ad73e
DannieSYD <18538999188@163.com>
2025-05-23 15:33:59 +0800

    updated in scripts 2

--- run_cov3cl_gain.bash
+++ run_cov3cl_gain.bash
@@ -1,18 +1,4 @@
 #!/bin/bash
-#SBATCH --job-name="MARCEL-benchmark"
-#SBATCH --output="result.%j.%N.out"
-#SBATCH --partition=gpuA100x4,gpuA100x8
-#SBATCH --mem=208G
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64   # spread out to use 1 core per numa, set to 64 if tasks is 1
-#SBATCH --constraint="scratch"
-#SBATCH --gpus-per-node=4
-#SBATCH --gpu-bind=closest   # select a cpu close to gpu on pci bus topology
-#SBATCH --account=bccg-delta-gpu
-#SBATCH --exclusive  # dedicated node for this job
-#SBATCH --no-requeue
-#SBATCH -t 48:00:00
 
 
 PORT=$((10000 + (RANDOM % 10001)))
